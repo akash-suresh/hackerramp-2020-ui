493d76961f7359cec5fdd7c240a01b5605182c97
akashsuresh96 <akash.s1@myntra.com>
2018-06-15 03:58:34 +0530

    2

--- app/screens/Home/index.js
+++ app/screens/Home/index.js
@@ -17,38 +17,41 @@ export default class Home extends Component {
 
   render() {
     return (
-      <section className="container home">
-        
-        <h3 className="The-Reviewer-Home">Critico</h3>
-        <Autocomplete
-          items={[  
-            { id: 1, label: 'Oneplus 6'},
-            { id: 2, label: 'Oneplus 5T'},
-            { id: 3, label: 'Redmi Note 5'},
-            { id: 4, label: 'Redmi Note 5 pro'},
-            { id: 5, label: 'Google Pixel 2XL'},
-            { id: 6, label: 'GoPro Hero'},
-            { id: 7, label: 'Canon EOS Mark 5D'},
-            { id: 8, label: 'Lenovo Thinkpad'},
-            { id: 9, label: 'Fitbit Charge 2'},
-            { id: 10, label: 'Apple iPhoneX'},
-          ]}
-          shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
-          getItemValue={item => item.label}
-          renderItem={(item, highlighted) =>
-            <div className="home-item"
-              key={item.id}
-              style={{ backgroundColor: highlighted ? '#eee' : 'transparent', textAlign: 'left', marginLeft: '5px', width:'300px'}}
-            >
-              {item.label}
-            </div>
-          }
-          value={this.state.value}
-          onChange={e => this.setState({ value: e.target.value })}
-          onSelect={this.handleSubmit}
-        />
-        
-      </section>
+      <div style={{height:'804px',backgroundImage:'url("http://3.bp.blogspot.com/-LRFlxajCKS4/Vexp3jb_fEI/AAAAAAAAYGE/m82WYWe3BXA/s1600/wood-background-3780-1920x1080-wallemo.jpg")'}}>
+        <section className="container home">
+          
+          <h3 className="The-Reviewer-Home">Critico</h3>
+          <Autocomplete
+            // style={{marginLeft:'260px', width: '525px', height:'40px'}}
+            items={[  
+              { id: 1, label: 'Oneplus 6'},
+              { id: 2, label: 'Oneplus 5T'},
+              { id: 3, label: 'Redmi Note 5'},
+              { id: 4, label: 'Redmi Note 5 pro'},
+              { id: 5, label: 'Google Pixel 2XL'},
+              { id: 6, label: 'GoPro Hero'},
+              { id: 7, label: 'Canon EOS Mark 5D'},
+              { id: 8, label: 'Lenovo Thinkpad'},
+              { id: 9, label: 'Fitbit Charge 2'},
+              { id: 10, label: 'Apple iPhoneX'},
+            ]}
+            shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+            getItemValue={item => item.label}
+            renderItem={(item, highlighted) =>
+              <div className="home-item"
+                key={item.id}
+                style={{ backgroundColor: highlighted ? '#eee' : 'transparent', textAlign: 'left', marginLeft: '5px', width:'300px'}}
+              >
+                {item.label}
+              </div>
+            }
+            value={this.state.value}
+            onChange={e => this.setState({ value: e.target.value })}
+            onSelect={this.handleSubmit}
+          />
+          
+        </section>
+      </div>
     );
   }
 }
